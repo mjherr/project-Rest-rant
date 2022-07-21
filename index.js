a1d4fb4600e0cd4a2153d1bf2bf45274e2b2b1c5
Maria Rios <kanelapr@gmail.com>
2022-07-21 15:37:31 -0400

    Fixed errors 404 page

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
+//Dependencies
 require('dotenv').config()
 
 const express = require('express')
 const app = express()
 
-//Homepage
+//Settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
-
-//Places route
+//Routes
 app.use('/places', require('./controllers/places'))
 
-//404 Page
-app.get('*', (req, res) => {
-    res.status(404).render('error404')
+app.get('/', (req, res) => {
+  res.render('home')
 })
 
+app.get('*', (req, res) => {
+  res.render('error404')
+})
+
+//Listen
 app.listen(process.env.PORT)
--- views/default.jsx
+++ views/default.jsx
@@ -12,3 +12,5 @@ function Def (html) {
         </html>
     )
 }
+
+module.exports = Def
