cdccaf1b9851e3765e323067743ae4219997ba87
Maria Rios <kanelapr@gmail.com>
2022-07-21 20:20:11 -0400

    added images

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,9 +7,15 @@ function error404 () {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't finde this page!</p>
+                <div>
+                    <img src="/images/matrix-code.jpg" alt="Black screen with green code"/>
+                </div>
+                <div>
+                Photo by <a href="https://unsplash.com/@markusspiske?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Markus Spiske</a> on <a href="https://unsplash.com/s/photos/technology?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div> 
             </main>
         </Def>
     )
 }
 
-module.exports = error404
+module.exports = error404  
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/ice-cream-cone.jpg" alt="Double ice cream scoop cone"/>
+                </div>
+                <div>
+                    Photo by <a href="https://unsplash.com/@sadswim?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">ian dooley</a> on <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
