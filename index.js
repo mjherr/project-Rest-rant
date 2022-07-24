eadd150dc2c455105848e357d1592ffb1a3833e6
Maria Rios <kanelapr@gmail.com>
2022-07-24 11:10:40 -0400

    fixing other errors

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-//Dependencies
+//Modules and Globals
 require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 
-//Settings
+// Express Settings
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
 
 //Routes
 app.use('/places', require('./controllers/places'))
